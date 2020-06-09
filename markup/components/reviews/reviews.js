691f117a14f3c5d0363b217b8985ee5041fad61c
denibudeyko <denbudeyko@gmail.com>
2020-06-09 08:18:08 +0300

    reviews and etc

--- markup/components/reviews/reviews.js
+++ markup/components/reviews/reviews.js
@@ -3,7 +3,17 @@ $(document).ready(function(){
   var $revSlides = $('.reviews-slides')
   $revSlides.slick({
 
-    slidesToShow: 5
+    slidesToShow: 5,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+
+        }
+      },
+      
+    ]
 
   })
 
