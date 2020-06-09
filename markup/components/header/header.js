35e7a5b0fe07f32a691490d00ee3a94c89783c3d
denibudeyko <denbudeyko@gmail.com>
2020-06-09 11:40:26 +0300

    fix

--- markup/components/header/header.js
+++ markup/components/header/header.js
@@ -7,4 +7,41 @@ $(document).ready(function(){
 
   })
 
+  
+
+
 })
+
+$(document).ready(function(){
+
+  const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true
+  });
+  
+  function updateScroll(){
+
+    scroll.update()
+
+  }
+  setTimeout(updateScroll, 100)
+
+
+  $('nav.menu a').on('click',function(event){
+    event.preventDefault();
+    var href = $(this).attr('href')
+    var element = $(href)[0];
+    scroll.scrollTo(element)
+    if(window.innerWidth < 768){
+      $('.header-menu').removeClass('active')
+      $('#header-burger').children('.hamburger').removeClass('is-active')
+    }
+  });
+})
+
+
+$(window).on('load', function () {
+
+  $('body').addClass('loaded')
+
+});
